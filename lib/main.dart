ec6a5b0499c009190a75623d01c14ab564fb4a6f
Steellow <hankilapio@gmail.com>
2020-01-14 18:10:32 +0200

    added screenshots

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ class MyApp extends StatelessWidget {
     ]);
 
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: Colors.redAccent,
       ),
